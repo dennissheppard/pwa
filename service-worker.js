3a6f74349a1fec50f5584c6732b55b2d933fb138
dennissheppard <dennis.sheppard@gmail.com>
2017-07-05 22:43:11 -0500

    Caching all of our new pirate goodies!

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,13 @@
-self.addEventListener('install', function(event) {
-  console.log(event);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('version1').then((cache) => {
+      return cache.addAll(
+        [
+          '/pirates.html',
+          '/styles/pirates.css',
+          '/styles/pirate.ttf',
+          '/images/i-love-pirates.jpg'
+        ]);
+    })
+  );
 });
