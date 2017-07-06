31e06f6068741b6739fd4176f27b405927c10778
dennissheppard <dennis.sheppard@gmail.com>
2017-07-05 22:44:02 -0500

    Arrgh, let's use the cache we created, but make sure we have a backup plan. Arrgh!

--- service-worker.js
+++ service-worker.js
@@ -11,3 +11,11 @@ self.addEventListener('install', (event) => {
     })
   );
 });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
