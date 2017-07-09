5f1b410244d66b28512db3408f4ce5009684e34b
dennissheppard <dennis.sheppard@gmail.com>
2017-07-09 17:02:53 -0500

    create offline landing page

--- service-worker.js
+++ service-worker.js
@@ -3,19 +3,39 @@ self.addEventListener('install', (event) => {
     caches.open('version1').then((cache) => {
       return cache.addAll(
         [
+          'index.html',
           '/pirates.html',
           '/styles/pirates.css',
           '/styles/pirate.ttf',
-          '/images/i-love-pirates.jpg'
+          '/images/i-love-pirates.jpg',
+          'offline.html'
         ]);
     })
   );
 });
 
+
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  if(!navigator.onLine && event.request.url.indexOf('index.html') !== -1) {
+    event.respondWith(showOfflineLanding(event));
+  }
+  else {
+    event.respondWith(pullFromCache(event));
+  }
+
 });
+
+function showOfflineLanding(event) {
+  return caches.match(new Request('offline.html'));
+}
+
+function pullFromCache(event) {
+  return caches.match(event.request).then((response) => {
+    return response || fetch(event.request).then((response) => {
+        return caches.open('version1').then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+  });
+}
