cb2ce72cec16f5c181e2881b9072b549f51d757b
dennissheppard <dennis.sheppard@gmail.com>
2017-08-16 23:05:19 -0500

    http2-server updates move asset refs back down to bottom

--- http2-server/server.js
+++ http2-server/server.js
@@ -10,11 +10,45 @@ const cert = {
                 cert: fs.readFileSync('./localhost.cert')
              };
 
+const index = fs.readFileSync('../index.html');
+const css = fs.readFileSync('../styles/pirates.css');
+const pirateManager = fs.readFileSync('../scripts/pirate-manager.js');
+
 app.use(express.static('../'));
 
-app.get('*', (req, res) => {
-  res.status(200);
+app.get('/home', (req, res) => {
+
+    let cssResource = {
+      path: '/styles/pirates.css',
+      contentType: 'text/css',
+      file: css
+    };
+
+    let pirateManagerResource = {
+      path: '/scripts/pirate-manager.js',
+      contentType: 'application/javascript',
+      file: pirateManager
+    };
+
+    pushResource(res, cssResource);
+    pushResource(res, resource);
+
+    res.writeHead(200);
+    res.end(index);
 });
+
+function pushResource(res, resource) {
+  let stream = res.push(resource.path, {
+      req: {'accept': '**/*'},
+      res: {'content-type': resource.contentType}
+    });
+
+    stream.on('error', err => {
+      console.log(err);
+    });
+
+    stream.end(resource.file);
+}
 
 spdy.createServer(cert, app)
     .listen(port, console.log('Listening on port: ' + port));
