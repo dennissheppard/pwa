67eeb136c1be7ffd1a71a569b3768db738b2a5d0
dennissheppard <dennis.sheppard@gmail.com>
2017-09-29 23:29:30 -0500

    some formatting tweaks declare timeout

--- script.js
+++ script.js
@@ -1,104 +1,98 @@
 (() => {
-  document.addEventListener('DOMContentLoaded', init, false);
-
-  function init() {
-    registerServiceWorker();
-    addListeners();
-    getComments().then((commentList) => renderComments(commentList));
-  }
-
-  function registerServiceWorker() {
-    if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js').then((registration) => {
-        console.log(registration);
-      }, function(err) {
-          console.log(err);
-        });
-      });
-    } else {
-      console.log('No service worker support in this browser');
+    document.addEventListener('DOMContentLoaded', init, false);
+    let offlineTimeout;
+    function init() {
+        registerServiceWorker();
+        addListeners();
+        getComments().then((commentList) => renderComments(commentList));
     }
-  }
 
-  function getComments() {
-    return pirateManager.getComments()
-    .then((commentList) => commentList);
-  }
-  var timeout;
-
-  function postComment() {
-    document.getElementById('commentBtn').innerHTML = "Posting...";
-    localforage.setItem('comment',
-        document.getElementById('comment-text').value)
-            .then(() => submitPost());
-  }
-
-  function submitPost() {
-    if (navigator.serviceWorker) {
-        navigator.serviceWorker.ready.then((sw) => {
-          return sw.sync.register('post-comment')
-            .then((args) => {
-              timeout = setTimeout(() => {
-                localforage.getItem('comment').then((val) => {
-                  if(val) {
-                    document.getElementById('no-connection-message').style.display = "block";
-                    document.getElementById('commentBtn').innerHTML = "Leave a comment";
-                    document.getElementById('comment-text').value = "";
-                  } else {
-                    clearTimeout(timeout);
-                  }
+    function registerServiceWorker() {
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('service-worker.js').then((registration) => {
+                    console.log(registration);
+                }, function (err) {
+                    console.log(err);
                 });
-              }, 3000);
-            })
-            .catch((err) => {
-              console.log(err);
             });
-        });
-    } else {
-        pirateManager.postComment().then((data) => {
+        } else {
+            console.log('No service worker support in this browser');
+        }
+    }
+
+    function getComments() {
+        return pirateManager.getComments()
+            .then((commentList) => commentList);
+    }
+
+    function postComment() {
+        document.getElementById('commentBtn').innerHTML = "Posting...";
+        localforage.setItem('comment', document.getElementById('comment-text').value)
+            .then(() => submitPost());
+    }
+
+    function submitPost() {
+        if (navigator.serviceWorker) {
+            navigator.serviceWorker.ready.then((sw) => {
+                return sw.sync.register('post-comment')
+                    .then((args) => {
+                        offlineTimeout = setTimeout(() => {
+                            localforage.getItem('comment').then((val) => {
+                                document.getElementById('no-connection-message').style.display = "block";
+                                document.getElementById('commentBtn').innerHTML = "Leave a comment";
+                                document.getElementById('comment-text').value = "";
+                            });
+                        }, 3000);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            });
+        } else {
+            pirateManager.postComment().then((data) => {
+                document.getElementById('comment-text').value = "";
+                document.getElementById('commentBtn').innerHTML = "Leave a comment";
+                document.getElementById('no-connection-message').style.display = "none";
+                appendComment(document.getElementById('comments'), data);
+            });
+        }
+    }
+
+    function addListeners() {
+        document.getElementById('commentBtn').addEventListener('click', () => postComment());
+        if (!navigator.serviceWorker) {
+            return;
+        }
+        navigator.serviceWorker.addEventListener('message', (event) => {
+            clearTimeout(offlineTimeout);
             document.getElementById('comment-text').value = "";
             document.getElementById('commentBtn').innerHTML = "Leave a comment";
             document.getElementById('no-connection-message').style.display = "none";
-            appendComment(document.getElementById('comments'), data);
+            appendComment(document.getElementById('comments'), event.data);
         });
     }
-  }
 
-  function addListeners() {
-    document.getElementById('commentBtn').addEventListener('click', () => postComment());
-    if (!navigator.serviceWorker) {
-        return;
+    function resetElements() {
+        let comments = document.getElementById('comments');
+        comments.innerHTML = "";
     }
-    navigator.serviceWorker.addEventListener('message', (event) => {
-      clearTimeout(timeout);
-      document.getElementById('comment-text').value = "";
-      document.getElementById('commentBtn').innerHTML = "Leave a comment";
-      document.getElementById('no-connection-message').style.display = "none";
-      appendComment(document.getElementById('comments'), event.data);
-    });
-  }
 
-  function resetElements() {
-    let comments = document.getElementById('comments');
-    comments.innerHTML = "";
-  }
+    function renderComments(commentList) {
+        resetElements();
+        let comments = document.getElementById('comments');
+        Object.keys(commentList).forEach((key) => {
+            let comment = commentList[key];
+            appendComment(comments, comment);
+        });
+    }
 
-  function renderComments(commentList) {
-    resetElements();
-    let comments = document.getElementById('comments');
-    Object.keys(commentList).forEach((key) => {
-      let comment = commentList[key];
-      appendComment(comments, comment);
-    });
-  }
-
-  function appendComment(commentsEl, comment) {
-    let commentElement = document.createElement('p');
-    commentElement.innerHTML = comment.commentText + " - " + comment.date;
-    commentsEl.appendChild(commentElement);
-    let hrElement = document.createElement('hr');
-    commentsEl.appendChild(hrElement);
-  }
+    function appendComment(commentsEl, comment) {
+        let commentElement = document.createElement('p');
+        commentElement.innerHTML = comment.commentText + " - " + comment.date;
+        commentsEl.appendChild(commentElement);
+        let hrElement = document.createElement('hr');
+        commentsEl.appendChild(hrElement);
+    }
 
 })();
